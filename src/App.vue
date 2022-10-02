c0a4a126a7415a1e089260aff6d067906735d6cb
Domantas Petrauskas <domantas@zyro.com>
2022-10-03 00:02:15 +0300

    Add lint config

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,37 @@
 <script setup lang="ts">
-import 'mapbox-gl/dist/mapbox-gl.css';
-import mapboxgl from 'mapbox-gl';
-import { onMounted, ref } from 'vue';
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl";
+import { onMounted, ref } from "vue";
 
-const mapContainer = ref<HTMLElement | null>(null)
+const mapContainer = ref<HTMLElement | null>(null);
 
 onMounted(() => {
-  mapboxgl.accessToken = 'pk.eyJ1IjoiZmlzdG1lbmFydXRvIiwiYSI6ImNsOHJtZzlrZTBucXAzbm4xeDQ1N29lbXcifQ.5DrUh4lgXkHcv6x3hyyTjw';
-  console.log(mapContainer.value)
-  const map = new mapboxgl.Map({
-    container: mapContainer.value!,
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [25.2, 54.7],
-    zoom: 3,
-    projection: {
-      name: 'globe'
-    }
-  });
-  map.on('load', () => {
-    map.setFog({}); // Set the default atmosphere style
-  });
-})
+	mapboxgl.accessToken =
+		"pk.eyJ1IjoiZmlzdG1lbmFydXRvIiwiYSI6ImNsOHJtZzlrZTBucXAzbm4xeDQ1N29lbXcifQ.5DrUh4lgXkHcv6x3hyyTjw";
+	console.log(mapContainer.value);
+	const map = new mapboxgl.Map({
+		container: mapContainer.value!,
+		style: "mapbox://styles/mapbox/streets-v11",
+		center: [25.2, 54.7],
+		zoom: 3,
+		projection: {
+			name: "globe",
+		},
+	});
+	map.on("load", () => {
+		map.setFog({}); // Set the default atmosphere style
+	});
+});
 </script>
 
 <template>
-  <div ref="mapContainer" class="map-container"></div>
+	<div ref="mapContainer" class="map-container"></div>
 </template>
 
 <style scoped>
 .map-container {
-  width: 100%;
-  height: 100%;
+	width: 100%;
+	height: 100%;
 }
 </style>
 
@@ -38,7 +39,7 @@ onMounted(() => {
 html,
 body,
 #app {
-  height: 100%;
-  margin: 0;
+	height: 100%;
+	margin: 0;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Unocss from 'unocss/vite'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Unocss from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Unocss()]
-})
+	plugins: [vue(), Unocss()],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+});
