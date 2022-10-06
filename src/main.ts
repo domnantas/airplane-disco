bb6c9cb97962b275cc82fbb696d2a461afa3f799
Domantas Petrauskas <domantas@zyro.com>
2022-10-07 02:31:15 +0300

    Add Daisy

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "@unocss/reset/tailwind.css";
+import "@kidonng/daisyui/index.css";
 import "uno.css";
 
 createApp(App).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,17 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Unocss from "unocss/vite";
+import { presetUno, transformerDirectives } from "unocss";
+import presetDaisy from "unocss-preset-daisy";
 
 export default defineConfig({
-	plugins: [vue(), Unocss()],
+	plugins: [
+		vue(),
+		Unocss({
+			transformers: [transformerDirectives()],
+			presets: [presetUno(), presetDaisy()],
+		}),
+	],
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
