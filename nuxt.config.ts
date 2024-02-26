77127e0d2e63cf8637abdd8dfe652827eb7c380f
domnantas <hello@domnantas.lt>
2024-02-26 21:50:58 +0200

    Test env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
   runtimeConfig: {
     aircraftScatterApiKey: "",
-    testing: "",
+    apiKey: "",
   },
   devtools: {
     enabled: true,
--- server/api/aircraft.get.ts
+++ server/api/aircraft.get.ts
@@ -7,7 +7,7 @@ const coordinatesSchema = z.object({
 });
 
 export default defineEventHandler(async (event) => {
-  const { aircraftScatterApiKey, testing } = useRuntimeConfig(event);
+  const { aircraftScatterApiKey, apiKey } = useRuntimeConfig(event);
   const query = await getValidatedQuery(event, (query) =>
     coordinatesSchema.safeParse(query),
   );
@@ -23,7 +23,7 @@ export default defineEventHandler(async (event) => {
 
   const { latitude, longitude } = query.data;
 
-  console.log("envs", aircraftScatterApiKey, testing);
+  console.log("envs", aircraftScatterApiKey, apiKey);
 
   const data = await $fetch(
     `https://aircraftscatter.p.rapidapi.com/lat/${latitude}/lon/${longitude}/`,
